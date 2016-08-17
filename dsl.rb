a29a31e8b12e9691a9b5a6bff5e75db35841a6a7
Justyna Wojtczak <ask@getbadges.io>
2016-08-17 20:25:56 +0000

    Get and post methods

--- dsl.rb
+++ dsl.rb
@@ -1,28 +1,44 @@
 $datas = []
+
 class ABC
     attr_accessor :id, :urls
-    
+
     def initialize(id, &block)
       self.id = id
 	  self.urls = []
 	  instance_eval &block
 	end
-	
-	def url(url, options = {})
-	  url = {:url => url}
-	  options.each do |option|
-	  	url[option.first] = option.last
-	  end
-	  urls << url
+
+	def get(url, options = {})
+		request('GET', url, options)
 	end
-	
+
+	def post(url, options = {})
+		request('POST', url, options)
+	end
+
+	def request(method, url, options = {})
+		url = {:url => url}
+		options.each do |option|
+		  	url[option.first] = option.last
+		end
+		urls << url
+
+		case method
+		when 'GET'
+			puts url
+		when 'POST'
+		else
+		end
+	end
+
 	def add(datas)
 		datas << self
 	end
 end
 
-ABC.new("getbadges") do 
-	url 'http://getbadges.io', :respond => :permanent_redirect, :respond_to => 'https://getbadges.io'
-	url 'https://https://getbadges.io/image.svg', :have_mime_type => 'image/svg'
-	url 'https://getbadges.io', :body => {content: 3}, :respond_with => :ok
-end.add($datas)
+ABC.new("getbadges") do
+	get 'http://getbadges.io', :respond => :permanent_redirect, :respond_to => 'https://getbadges.io'
+	get 'https://https://getbadges.io/image.svg', :have_mime_type => 'image/svg'
+	post 'https://getbadges.io', :body => {content: 3}, :respond_with => :ok
+end
