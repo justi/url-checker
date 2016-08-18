64c0e95869c7516dc3bfdc00d5391e188d4be517
Justyna Wojtczak <ask@getbadges.io>
2016-08-18 22:30:53 +0000

    Add following redirects

--- dsl.rb
+++ dsl.rb
@@ -4,6 +4,8 @@ $datas = []
 
 class ABC
 	include HTTParty
+	follow_redirects false
+	MAX_REDIRECTS = 3
 
 	CODES_TO_OBJ = ::Net::HTTPResponse::CODE_CLASS_TO_OBJ.merge ::Net::HTTPResponse::CODE_TO_OBJ
     attr_accessor :id, :urls
@@ -22,6 +24,32 @@ class ABC
 		request('POST', url, options)
 	end
 
+	def follow_redirects(url)
+		redirects_count = 0
+		input_url = url
+		result = []
+		loop do
+			response = self.class.get(input_url)
+			result << { url: input_url, status: response.code.to_s, redirects: redirects_count}
+			input_url =	response.header['location']
+			redirects_count +=1
+			break unless input_url || redirects_count > MAX_REDIRECTS
+		end
+		return result
+	end
+
+	def get_response_respond_with(data)
+		data.last[:url]
+	end
+
+	def get_response_status_code(data)
+		data.last[:status]
+	end
+
+	def get_response_redirects_count(data)
+		data.last[:redirects]
+	end
+
 	def request(method, url, options = {})
 		url = {:url => url}
 		options.each do |option|
@@ -32,11 +60,12 @@ class ABC
 		begin
 			case method
 			when 'GET'
-				response = self.class.get(url[:url])
-				result_ok &&= url[:respond_with] == response.code if url[:respond_with]
-				result_ok &&= url[:respond_to] == response.request.last_uri.to_s if url[:respond_to] && response.request.last_uri
-				puts "Response: #{response.code} #{CODES_TO_OBJ[response.code.to_s]}"
-				puts result_ok
+				results = follow_redirects(url[:url])
+				status_code = get_response_status_code(results)
+				respond_url = get_response_respond_with(results)
+				result_ok &&= url[:respond_with] == status_code if url[:respond_with]
+				result_ok &&= url[:respond_to] == respond_url if url[:respond_to]
+				puts "For #{url[:url]} response code is #{status_code} #{CODES_TO_OBJ[status_code]} and redirects: #{get_response_redirects_count(results)}"
 			when 'POST'
 				response = self.class.post(url[:url])
 				result_ok &&= url[:respond_with] == response.code if url[:respond_with]
@@ -58,6 +87,6 @@ end
 
 ABC.new("getbadges") do
 	get 'http://getbadg.es', :respond => :permanent_redirect, :respond_to => 'https://getbadges.io/'
-	#get 'https://getbadges.io/image.svg', :have_mime_type => 'image/svg'
+	get 'https://getbadges.io/image.svg', :have_mime_type => 'image/svg'
 	#post 'https://getbadges.io', :body => {content: 3}, :respond_with => 200
 end
