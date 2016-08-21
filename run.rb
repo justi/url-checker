e4074213963cea7bdaf04e69a957f7fb66963d85
Justyna Wojtczak <ask@getbadges.io>
2016-08-21 13:18:41 +0000

    Example

--- run.rb
+++ run.rb
@@ -1,9 +1,16 @@
-require_relative 'dsl.rb'
+require_relative 'dsl'
 
+# For tests, run:
+# ruby run.rb example.txt
 filename = ARGV[0]
-data = File.read(filename) if filename
-ABC.new("getbadges") do
-	eval(data)
-	#get 'https://getbadges.io/image.svg', :have_mime_type => 'image/svg'
-	#post 'https://getbadges.io', :body => {content: 3}, :respond_with => 200
-end.display_results()
+
+if filename
+	data = File.read(filename)
+	ABC.new("getbadges") do
+		eval(data)
+		#get 'https://getbadges.io/image.svg', :have_mime_type => 'image/svg'
+		#post 'https://getbadges.io', :body => {content: 3}, :respond_with => 200
+	end.display_results()
+else
+	puts "Please type: ruby run.rb input_txt_file"
+end
